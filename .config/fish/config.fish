6f5ea04a97c4b6e5a42f511d142301ab3c60825d
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2021-07-14 21:36:54 -0500

    wallpaper

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,4 @@ end
 set -g EDITOR 'nvim'
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH # ghcup-env
 set -g FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git"
+alias db="dotbare"
