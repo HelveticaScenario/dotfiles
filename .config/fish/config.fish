ec340a1514f2978ba165e7b7b714ac4b17e94ef8
Daniel Lewis <daniel.lewis.software.engineer@gmail.com>
2021-07-15 21:17:24 -0500

    keyring

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -g EDITOR 'nvim'
+set -Ux EDITOR 'nvim'
+set -Ux VISUAL 'code'
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $HOME/.ghcup/bin $PATH # ghcup-env
 set -g FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --exclude .git"
 alias db="dotbare"
+
+if test -n "$DESKTOP_SESSION"
+    set -x (gnome-keyring-daemon --start | string split "=")
+end
